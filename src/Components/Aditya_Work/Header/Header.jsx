0023f69c32c8492e740f4ce2be1ff651041668f9
frozenopertor <adityagupta11112002@gmail.com>
2024-01-30 21:46:06 +0530

    Second commit

--- src/Components/Aditya_Work/Header/Header.jsx
+++ src/Components/Aditya_Work/Header/Header.jsx
@@ -1,31 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { NavLink } from "react-router-dom";
+
 function Header() {
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!isMobileMenuOpen);
+  };
+
   return (
     <>
       <header className="rounded-xl shadow sticky z-50 top-0 bg-gradient-to-b from-gray-900 to-gray-600">
-        <nav className=" border-gray-200 px-4 lg:px-6 py-2.5">
+        <nav className="border-gray-200 px-4 lg:px-6 py-2.5 sm:flex-wrap">
           <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
             <Link to="/" className="flex items-center">
               <h1 className="text-2xl text-orange-400 font-bold">Hunkaar</h1>
             </Link>
-            <div className="flex items-center lg:order-2">
-              {/* <Link
-                to="#"
-                className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+            <div className="lg:hidden">
+              <button
+                onClick={toggleMobileMenu}
+                className="text-white focus:outline-none"
               >
-                Log in
-              </Link>
-              <Link
-                to="#"
-                className="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
-              >
-                Get started
-              </Link> */}
+                {isMobileMenuOpen ? "Close" : "Menu"}
+              </button>
             </div>
             <div
-              className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
+              className={`${
+                isMobileMenuOpen ? "block" : "hidden"
+              } lg:flex justify-between items-center w-full lg:w-auto lg:order-1`}
               id="mobile-menu-2"
             >
               <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0 justify-between">
@@ -88,7 +91,7 @@ function Header() {
                   >
                     Sponsors
                   </NavLink>
-                </li>  
+                </li>
                 <li className="text-white hover:text-lg mr-10">
                   <NavLink
                     to="/merch"
@@ -100,7 +103,7 @@ function Header() {
                   >
                     Merchandise
                   </NavLink>
-                </li>    
+                </li>
                 <li className="text-white hover:text-lg mr-10">
                   <NavLink
                     to="/admin"
@@ -112,7 +115,7 @@ function Header() {
                   >
                     Admin
                   </NavLink>
-                </li>            
+                </li>
               </ul>
             </div>
           </div>
